5929ceacf54544b3a4403b5200fcc62fa70675a7
Emil Pausz <emilpausz@gmail.com>
2018-06-09 14:27:54 +0200

    Poprawka kafelkow, menu, aktywne maila, listy

--- es6/main.js
+++ es6/main.js
@@ -22,12 +22,17 @@
 // Smoth Scroll
 (function(){
     // Select all links with hashes
+
+    const menu = $('.page-header-nav');
+
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
   .click(function(event) {
     // On-page links
+    console.log(menu);
+    $(menu).toggleClass('expanded');
     if (
       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
       && 
